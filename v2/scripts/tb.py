453b3b71e938d6d6fbaf5e7b50ece1d2c83e0a9e
BalasaiAnish <ponnaluribalasai@gmail.com>
2025-04-14 13:23:26 +0530

    changed testbench generator

---
+++ v2/scripts/tb.py
@@ -0,0 +1,144 @@
+import os
+import sys
+import re
+
+input_file = sys.argv[1]
+
+inputs = dict()
+outputs = dict()
+params = list()
+
+tab = " " * 4
+
+with open(input_file,'r') as file:
+	lines = file.readlines()
+
+	for i in range(len(lines)):
+		current_line = lines[i]
+
+		# input
+		if "input" in current_line:
+			# remove input from current line
+			current_line = re.sub(r"input ","",current_line).strip()
+			# vector input
+			if "[" in current_line:
+				start = current_line.find("[")
+				end = current_line.find("]")
+				input_len = current_line[start:end+1]
+				current_line = current_line[end+1:].strip()
+				# remove last comma if present
+				current_line = current_line.rstrip(",")
+				ports = current_line.split(",")
+
+				for j in ports:
+					inputs[j] = input_len
+
+			# scalar input
+			else:
+				current_line = current_line.rstrip(",")
+				ports = current_line.split(",")
+
+				for j in ports:
+					inputs[j] = ""
+
+
+		# outputs
+		elif "output" in current_line:
+			# remove input from current line
+			current_line = re.sub(r"output ","",current_line).strip()
+
+			# remove reg keyword if present
+			if "reg" in current_line:
+				current_line = re.sub(r"reg ","",current_line)
+			# vector output
+			if "[" in current_line:
+				start = current_line.find("[")
+				end = current_line.find("]")
+				output_len = current_line[start:end+1]
+				current_line = current_line[end+1:].strip()
+				current_line = current_line.rstrip(",")
+				ports = current_line.split(",")
+
+				for j in ports:
+					outputs[j] = output_len
+
+			# scalar input
+			else:
+				current_line = current_line.rstrip(",")
+				ports = current_line.split(",")
+
+				for j in ports:
+					outputs[j] = ""
+
+		elif 'parameter' in current_line:
+			current_line = re.sub(r"parameter ","",current_line).strip()
+			current_line = current_line.rstrip(",")
+			params.append(current_line)
+
+
+output_file = sys.argv[2]
+
+module_name = input_file.split("/")[-1].strip()
+module_name = re.sub(r".v","",module_name)
+
+with open(output_file,'w') as file:
+	new_lines = list()
+	new_lines.append(f"module {module_name}();\n")
+
+	# write params
+
+	for i in params:
+		new_lines.append("parameter " + i + ";\n")
+
+	for i in inputs.keys():
+		# vector
+		if(inputs[i]):
+			new_lines.append(f"reg {inputs[i]} {i};\n")
+
+		else:
+			new_lines.append(f"reg {i};\n")
+
+	for i in outputs.keys():
+		# vector
+		if(outputs[i]):
+			new_lines.append(f"wire {outputs[i]} {i};\n")
+
+		else:
+			new_lines.append(f"wire {i};\n")
+
+	new_lines.append(f"{module_name} dut(\n")
+
+	for i in inputs.keys():
+		new_lines.append(f".{i}({i}),\n")
+
+	ops = list(outputs.keys())
+	for i in ops:
+		if i != ops[-1]:
+			new_lines.append(f".{i}({i}),\n")
+
+	new_lines.append(");\n")
+
+	new_lines.append("initial begin\n")
+
+	for i in inputs.keys():
+		new_lines.append(f"{i} = 0;\n")
+
+	new_lines.append("end\n")
+
+	if "clk" in inputs.keys():
+		new_lines.append(f"always\n{tab}#5 clk = ~clk;\n")
+
+	new_lines.append("// Write testbench code here\n")
+	new_lines.append("initial begin\n\n")
+	new_lines.append("end\n")
+
+	new_lines.append(f'initial begin\n$dumpfile("dumpfile.vcd");\n$dumpvars(0,{module_name}_tb);\nend\n')
+
+	new_lines.append("endmodule")
+
+	file.writelines(new_lines)
+
+
+print(inputs)
+print(outputs)
+print(params)
--- v2/test/pl_stages/fetch/mt_pc_tb.v
+++ v2/test/pl_stages/fetch/mt_pc_tb.v
@@ -1,114 +1,38 @@
-module mt_pc_tb();
+module mt_pc();
 parameter NUM_THREADS = 8;
 parameter BITS_THREADS = $clog2(NUM_THREADS);
 parameter ADDRESS_WIDTH = 32;
-reg clk,rst;
+reg clk;
+reg rst;
 reg [BITS_THREADS-1:0] tid;
 reg pc_src_e;
 reg [BITS_THREADS-1:0] branch_tid_e;
 reg [ADDRESS_WIDTH-1:0] pc_target_e;
 wire [ADDRESS_WIDTH-1:0] pc;
-
 mt_pc dut(
-.clk(clk),.rst(rst),
+.clk(clk),
+.rst(rst),
 .tid(tid),
 .pc_src_e(pc_src_e),
 .branch_tid_e(branch_tid_e),
 .pc_target_e(pc_target_e),
-.pc(pc)
 );
-
-
 initial begin
-    $dumpfile("dumpfile.vcd");
-    $dumpvars(0, mt_pc2_tb);
+clk = 0;
+rst = 0;
+tid = 0;
+pc_src_e = 0;
+branch_tid_e = 0;
+pc_target_e = 0;
 end
-
-initial begin
-clk=1'b0;
-rst=1'b1;
-tid=3'b000;
-pc_src_e=1'b0;
-branch_tid_e=3'b000;
-pc_target_e=32'd0;
-end
-
-// Write testbench code here
-initial begin
-    #10
-    rst=0;
-    // write PC vals
-    pc_src_e=1'b0;
-    branch_tid_e=3'd0;
-    pc_target_e=32'd100;
-
-    #10
-    pc_src_e=1'd0;
-    branch_tid_e=3'd1;
-    pc_target_e=32'd200;
-
-    #10
-    pc_src_e=1'd0;
-    branch_tid_e=3'd2;
-    pc_target_e=32'd300;
-
-    #10
-    pc_src_e=1'd0;
-    branch_tid_e=3'd3;
-    pc_target_e=32'd400;
-
-    #10
-    pc_src_e=1'd1;
-    branch_tid_e=3'd4;
-    pc_target_e=32'd100;
-
-    #10
-    pc_src_e=1'd1;
-    branch_tid_e=3'd5;
-    pc_target_e=32'd100;
-
-    #10
-    pc_src_e=1'd1;
-    branch_tid_e=3'd6;
-    pc_target_e=32'd100;
-
-    #10
-    pc_src_e=1'd1;
-    branch_tid_e=3'd7;
-    pc_target_e=32'd100;
-
-    #10
-    tid=3'd0;
-
-    #10
-    tid=3'd1;
-
-    #10
-    tid=3'd2;
-
-    #10
-    tid=3'd3;
-
-    #10
-    tid=3'd4;
-
-    #10
-    tid=3'd5;
-
-    #10
-    tid=3'd6;
-
-    #10
-    tid=3'd7;
-
-end
-
-initial begin
-    #1000 $finish();
-end
-
-
 always
     #5 clk = ~clk;
+// Write testbench code here
+initial begin
 
+end
+initial begin
+$dumpfile("dumpfile.vcd");
+$dumpvars(0,mt_pc_tb);
+end
 endmodule
