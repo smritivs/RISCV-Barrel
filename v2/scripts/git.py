6f70d24c81e3d7e618293c3f77c8e42eb70d7496
BalasaiAnish <ponnaluribalasai@gmail.com>
2025-04-16 19:05:41 +0530

    added python scripts

--- v2/scripts/git.py
+++ v2/scripts/git.py
@@ -7,6 +7,6 @@ branch = sys.argv[2]
 
 if __name__ == "__main__":
 	os.system("git add . -A")
-	os.system(f"git commit -m {msg}")
+	os.system(f"git commit -m \"{msg}\"")
 	os.system(f"xclip -sel c < {git_key_path}")
 	os.system(f"git push origin {branch}")
