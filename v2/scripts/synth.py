0fdd91145dd53beec2a8c3b08062a8c68ca493d5
BalasaiAnish <ponnaluribalasai@gmail.com>
2025-05-06 12:01:28 +0530

    added synthesis support using yosys

---
+++ v2/scripts/synth.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import glob
+import subprocess
+from const import sky_path
+
+top_module = sys.argv[1]
+
+if __name__ == "__main__":
+	file_list = glob.glob("./src/*.v")
+
+	with open("./scripts/file_list.txt","w") as file:
+		for i in file_list:
+			file.writelines(i + "\n")
+
+	with open("synth.ys","w") as file:
+		lines = list()
+		lines.append(f"read_liberty -lib {sky_path}")
+		lines.append("read_verilog_file_list -f ./scripts/file_list.txt")
+		lines.append(f"synth -top {top_module}")
+		lines.append("flatten")
+		lines.append(f"abc -liberty {sky_path}")
+		file.writelines(lines)
+
+	os.system("/bin/bash -c \"source ~/oss-cad-suite/environment && yosys ./scripts/synth.ys\"")
