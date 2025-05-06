002005f291f5f02d4f8ab3fa808c559476684cb7
BalasaiAnish <ponnaluribalasai@gmail.com>
2025-05-06 12:02:52 +0530

    bugfix

--- v2/scripts/synth.py
+++ v2/scripts/synth.py
@@ -13,13 +13,13 @@ if __name__ == "__main__":
 		for i in file_list:
 			file.writelines(i + "\n")
 
-	with open("synth.ys","w") as file:
+	with open("./scripts/synth.ys","w") as file:
 		lines = list()
-		lines.append(f"read_liberty -lib {sky_path}")
-		lines.append("read_verilog_file_list -f ./scripts/file_list.txt")
-		lines.append(f"synth -top {top_module}")
-		lines.append("flatten")
-		lines.append(f"abc -liberty {sky_path}")
+		lines.append(f"read_liberty -lib {sky_path}\n")
+		lines.append("read_verilog_file_list -f ./scripts/file_list.txt\n")
+		lines.append(f"synth -top {top_module}\n")
+		lines.append("flatten\n")
+		lines.append(f"abc -liberty {sky_path}\n")
 		file.writelines(lines)
 
 	os.system("/bin/bash -c \"source ~/oss-cad-suite/environment && yosys ./scripts/synth.ys\"")
