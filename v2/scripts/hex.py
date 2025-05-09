e3a8f27efc39769ff03fef020ad6f0033952f0b7
BalasaiAnish <ponnaluribalasai@gmail.com>
2025-05-09 10:52:49 +0530

    updated hex and testbench scripts

--- v2/scripts/hex.py
+++ v2/scripts/hex.py
@@ -9,8 +9,13 @@ else:
 	input_files = "./scripts/test.c"
 
 if __name__ == "__main__":
-	os.system(f"riscv32-unknown-elf-gcc {input_files} -o temp.elf")
+	if(".s" in input_files):
+		os.system(f"riscv32-unknown-elf-as {input_files} -o temp.elf")
+
+	elif(".c" in input_files):
+		os.system(f"riscv32-unknown-elf-gcc {input_files} -o temp.elf")
+
 	os.system("riscv64-unknown-elf-elf2hex --bit-width 32 --input temp.elf --output op.hex")
 	os.system("rm *.elf")
 	os.system("mv ./op.hex ./src/code.mem")
-	os.system("clear")
+	# os.system("clear")
